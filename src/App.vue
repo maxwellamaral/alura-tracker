5f03ca47528fd82bbe4ef7704c4a1f7c0d11b386
Maxwell Anderson <maxwell.amaral@ifpb.edu.br>
2022-11-11 13:27:23 -0300

    🎁Lista de tarefas cridas

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@
     </div>
     <div class="columm is-three-quarter">
       <FormularioHome />
+      <div class="lista">
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+      </div>
     </div>
   </main>
 </template>
@@ -13,16 +19,20 @@
 import { defineComponent } from 'vue';
 import BarraLateral from "@/components/BarraLateral.vue";
 import FormularioHome from "@/components/Formulario.vue";
+import Tarefa from "@/components/Tarefa.vue";
 
 export default defineComponent({
   name: 'App',
   components: {
     BarraLateral,
-    FormularioHome
+    FormularioHome,
+    Tarefa,
   }
 });
 </script>
 
 <style>
-
+.lista {
+  padding: 1.25rem;
+}
 </style>
